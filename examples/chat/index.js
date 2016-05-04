ad88fce00e6568970003528b33566d523715791a
amit3vr <amit3vr@gmail.com>
2016-05-05 00:13:15 +0300

    Updated example to match new server interface

--- examples/chat/index.js
+++ examples/chat/index.js
@@ -1,11 +1,17 @@
+'use strict';
+
 // Setup basic express server
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('../..')(server);
+var io = require('../..')();
 var port = process.env.PORT || 3000;
 
-server.listen(port, function () {
+
+io
+.attach(server)
+.listen(port, function()
+{
   console.log('Server listening at port %d', port);
 });
 
@@ -13,7 +19,6 @@ server.listen(port, function () {
 app.use(express.static(__dirname + '/public'));
 
 // Chatroom
-
 var numUsers = 0;
 
 io.on('connection', function (socket) {
